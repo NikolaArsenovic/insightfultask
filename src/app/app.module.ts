1423518fae886f2d54eb264d0cca9c4cb1cb80cd
Nikola Arsenovic <nikola@passion4business.de>
2023-12-29 21:55:26 +0100

    Project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from './core/core.module';
+import { Environment } from './core/models/environment.model';
+import { NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
-  providers: [],
+  providers: [
+    { provide: Environment, useValue: environment },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

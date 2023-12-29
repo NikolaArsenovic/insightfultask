094b5e2ed885ad6cc794a859cbfc7510c25ad856
Nikola Arsenovic <nikola@passion4business.de>
2023-12-29 23:37:18 +0100

    Api infrastructure added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { apiBackendProvider } from './interceptors/api.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [
+    apiBackendProvider
   ]
 })
 export class CoreModule { }
